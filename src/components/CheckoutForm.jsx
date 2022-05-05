f82a9966e851c002202b11ed71636e973dca4d57
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-05-05 19:35:49 +0200

    submit form

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,14 +1,22 @@
+import { useRef } from "react";
 function CheckoutForm() {
+  const formEl = useRef(null);
+  function submitted(e) {
+    e.preventDefault();
+    console.log(formEl.current.elements.name.value);
+    console.log(formEl.current.elements.email.value);
+  }
   return (
-    <form>
+    <form ref={formEl} onSubmit={submitted}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" />
+        <input required type="text" name="name" id="name" />
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" />
+        <input required type="email" name="email" id="email" />
       </div>
+      <button>Pay</button>
     </form>
   );
 }
